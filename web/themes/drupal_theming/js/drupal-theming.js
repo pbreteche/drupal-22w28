f7e8f1ca64fdec4e4f47956afee521e0d8b88257
Pierre Bretéché <pbreteche@dawan.fr>
2022-07-12 10:32:49 +0200

    création theme

---
+++ web/themes/drupal_theming/js/drupal-theming.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Formation Drupal Theming behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.drupalTheming = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
