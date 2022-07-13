9a811a1be8deaa2ee78a61264b380cd17f437142
Pierre Bretéché <pbreteche@dawan.fr>
2022-07-13 12:31:10 +0200

    block configurable

--- web/modules/formation3/src/Plugin/Block/ExampleBlock.php
+++ web/modules/formation3/src/Plugin/Block/ExampleBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\formation3\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides an example block.
@@ -15,14 +16,60 @@ use Drupal\Core\Block\BlockBase;
  */
 class ExampleBlock extends BlockBase {
 
+  public function defaultConfiguration() {
+    return [
+      'max elements' => 5,
+    ];
+  }
+
+  public function blockForm($form, FormStateInterface $formState) {
+    $config = $this->getConfiguration();
+
+    $form['max_elements'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Max elements'),
+      '#default_value' => $config['max elements'],
+    ];
+
+    return $form;
+  }
+
+  public function blockValidate($form, FormStateInterface $form_state) {
+    $maxElementsInput = $form_state->getValue('max_elements');
+    if ($maxElementsInput <= 0) {
+      $form_state->setErrorByName('max_elements', $this->t('Type value should greater than 0.'));
+    }
+  }
+
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->setConfigurationValue('max elements', $form_state->getValue('max_elements'));
+  }
+
   /**
    * {@inheritdoc}
    */
   public function build() {
+    /** @var \Drupal\Core\Database\Connection $connection */
+    $connection = \Drupal::service('database');
+    $config = $this->getConfiguration();
+
+    $result = $connection->select('formation3_example')
+      ->fields('formation3_example', ['id', 'uid', 'type', 'status', 'created'])
+      ->condition('status', 1)
+      ->range(0, $config['max elements'])
+      ->execute()
+    ;
+
+    $items = [];
+
+    foreach ($result as $row) {
+      $items[] = $row->type;
+    }
+
     $build['content'] = [
-      '#markup' => $this->t('It works!'),
+      '#theme' => 'item_list',
+      '#items' => $items,
     ];
     return $build;
   }
-
 }
